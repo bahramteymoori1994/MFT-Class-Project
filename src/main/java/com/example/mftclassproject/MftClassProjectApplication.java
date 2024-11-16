15dd41280838f85726472e8c2ecbccc100cf28b3
DESKTOP-VA5FTDD\Bahram <bahram.teymoori@outlook.com>
2024-11-16 20:10:50 +0330

    First Initialization

---
+++ src/main/java/com/example/mftclassproject/MftClassProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mftclassproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MftClassProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MftClassProjectApplication.class, args);
+    }
+
+}
