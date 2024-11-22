d0747fff8097378a09cf44b021d6e3b965c73f9b
sajjadchakameh <smsc7680@gmail.com>
2024-11-22 11:05:55 +0330

    updated

--- src/main/java/com/example/mftclassproject/lessonsession/controller/LessonSessionController.java
+++ src/main/java/com/example/mftclassproject/lessonsession/controller/LessonSessionController.java
@@ -1,7 +1,43 @@
 package com.example.mftclassproject.lessonsession.controller;
 
+import com.example.mftclassproject.classactivity.service.ClassActivityService;
+import com.example.mftclassproject.lessonsession.model.entity.LessonSession;
+import com.example.mftclassproject.lessonsession.service.LessonSessionService;
+import com.example.mftclassproject.mftclass.model.entity.MFTClass;
+import com.example.mftclassproject.mftclass.service.impl.MFTClassService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/LessonSessions")
 public class LessonSessionController {
+
+    private final LessonSessionService lessonSessionService;
+    private final MFTClassService mftClassService;
+    private final ClassActivityService classActivityService;
+
+
+    public LessonSessionController(LessonSessionService lessonSessionService, MFTClassService mftClassService, ClassActivityService classActivityService) {
+        this.lessonSessionService = lessonSessionService;
+        this.mftClassService = mftClassService;
+        this.classActivityService = classActivityService;
+    }
+
+
+    @GetMapping
+    public String loadLessonSessions(Model model) {
+        model.addAttribute("AllLessonSessions", lessonSessionService.findAll());
+        model.addAttribute("AllMFTClass", mftClassService.findAll());
+        model.addAttribute("AllClassActivity", classActivityService.findAll());
+        return "lessonSessions";
+    }
+
+    public String saveLessonSession(LessonSession lessonSession, Model model, @ModelAttribute("sessionNumber") int sessionNumber ) {
+
+        lessonSessionService.save(lessonSession);
+        return "redirect:/lessonSessions";
+    }
 }
